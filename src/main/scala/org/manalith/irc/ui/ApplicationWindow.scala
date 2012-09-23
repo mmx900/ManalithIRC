738c6a02c5a31000f51efe4f6317a94a65340dd9
mmx900 <mmx900@gmail.com>
2012-09-23 21:25:04 +0900

    창 종료시 Exception 해결

--- src/main/scala/org/manalith/irc/ui/ApplicationWindow.scala
+++ src/main/scala/org/manalith/irc/ui/ApplicationWindow.scala
@@ -1,44 +1,41 @@
 package org.manalith.irc.ui;
 
 import java.util.ArrayList
+
 import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.mutable.Subscriber
+import scala.collection.mutable.Publisher
+
+import org.eclipse.e4.xwt.annotation.UI
 import org.eclipse.e4.xwt.IConstants
 import org.eclipse.e4.xwt.XWT
-import org.eclipse.e4.xwt.annotation.UI
-import org.eclipse.swt.SWT
+import org.eclipse.jface.dialogs.MessageDialog
 import org.eclipse.swt.custom.CTabFolder
+import org.eclipse.swt.custom.CTabFolder2Adapter
+import org.eclipse.swt.custom.CTabFolderEvent
 import org.eclipse.swt.custom.CTabItem
+import org.eclipse.swt.events.DisposeEvent
+import org.eclipse.swt.events.DisposeListener
 import org.eclipse.swt.widgets.Event
 import org.eclipse.swt.widgets.Shell
-import org.manalith.irc.ManalithIRC
+import org.eclipse.swt.SWT
+import org.manalith.irc.helper.LogHelper
+import org.manalith.irc.helper.SwtUtil
 import org.manalith.irc.model.Connection
+import org.manalith.irc.model.ConnectionManager
 import org.manalith.irc.model.Server
-import org.pircbotx.Channel
-import org.eclipse.e4.xwt.jface.XWTDialog
-import org.eclipse.jface.dialogs.InputDialog
-import org.apache.commons.lang3.StringUtils
-import scala.collection.mutable.Publisher
-import scala.collection.mutable.Subscriber
-import org.eclipse.swt.widgets.Display
+import org.manalith.irc.ManalithIRC
 import org.pircbotx.hooks.events.JoinEvent
 import org.pircbotx.hooks.events.UserListEvent
 import org.pircbotx.hooks.ListenerAdapter
-import org.pircbotx.hooks.events.TopicEvent
+import org.pircbotx.Channel
 import org.pircbotx.PircBotX
-import org.manalith.irc.model.ConnectionManager
-import org.manalith.irc.helper.SwtUtil
-import org.eclipse.swt.events.DisposeListener
-import org.eclipse.swt.events.DisposeEvent
-import org.eclipse.swt.custom.CTabFolder2Adapter
-import org.eclipse.swt.custom.CTabFolderEvent
-import org.eclipse.jface.dialogs.MessageDialog
-import org.manalith.irc.helper.LogHelper
 
 class ApplicationWindow extends Publisher[Action] with LogHelper {
 	private val EVENT_WINDOW_DISPOSED = "WindowDisposed";
 	private val EVENT_CONNECT_BUTTON_CLICKED = "ConnectButtonClicked";
 
-	lazy val channelViewList = new ArrayList[ChannelView]();
+	lazy val channelTabs = new ArrayList[CTabItem]();
 
 	subscribe(new ApplicationWindowActionListener());
 
@@ -49,9 +46,10 @@ class ApplicationWindow extends Publisher[Action] with LogHelper {
 	private var tabFolder: CTabFolder = null;
 
 	def getChannelView(channelName: String): ChannelView = {
-		for (view <- channelViewList) {
-			if (view.channel.getName() == channelName) {
-				return view;
+		//return channelViewList.filter(v => v.channel.getName() == channelName);
+		for (tab <- channelTabs) {
+			if (tab.getControl().asInstanceOf[ChannelView].channel.getName() == channelName) {
+				return tab.getControl().asInstanceOf[ChannelView];
 			}
 		}
 
@@ -83,19 +81,9 @@ class ApplicationWindow extends Publisher[Action] with LogHelper {
 				//quit 메시지를 보내기 위해 관련 탭을 닫기 전 먼저 연결을 끊는다.
 				connection.disconnect;
 
-				val toClose = new ArrayList[ChannelView]()
-
-				for (view <- channelViewList) {
-					if (view.connection == connection) {
-						//java.util.ConcurrentModificationException 방지
-						toClose.add(view);
-					}
-				}
-
-				for (view <- toClose) {
-					//view를 포함하는 CTabItem을 dispose
-					view.getParent().dispose();
-				}
+				channelTabs
+					.filter(t => t.getControl().asInstanceOf[ChannelView].connection == connection)
+					.foreach(t => t.dispose())
 			}
 		});
 
@@ -103,22 +91,24 @@ class ApplicationWindow extends Publisher[Action] with LogHelper {
 	}
 
 	def createChannelTab(channel: Channel, connection: Connection): CTabItem = {
-		val item = new CTabItem(tabFolder, SWT.CLOSE);
-		item.setText(channel.getName());
+		val tab = new CTabItem(tabFolder, SWT.CLOSE);
+		tab.setText(channel.getName());
 		val view = new ChannelView(tabFolder, SWT.NONE, channel,
 			connection);
-		item.setControl(view);
-		tabFolder.setSelection(item);
-		channelViewList.add(view);
+		tab.setControl(view);
+		tabFolder.setSelection(tab);
+		channelTabs += tab;
 
-		item.addDisposeListener(new DisposeListener() {
+		tab.addDisposeListener(new DisposeListener() {
 			def widgetDisposed(event: DisposeEvent) = {
-				connection.partChannel(channel);
-				channelViewList.remove(view);
+				if (connection.isConnected)
+					connection.partChannel(channel);
+
+				channelTabs -= tab;
 			}
 		});
 
-		return item;
+		return tab;
 	}
 
 	def closeTab(tab: IrcTab) {
@@ -161,8 +151,8 @@ class ApplicationWindow extends Publisher[Action] with LogHelper {
 					for (s <- servers) {
 						val channels = new ArrayList[String]();
 						for (c <- s.configurationsAt("channels.channel")) {
-							channels.add(c.getString("name"));
-							//channels.add(c.getString("password"));
+							channels += c.getString("name");
+							//channels += c.getString("password");
 						}
 
 						val server = new Server(s.getString("host"),
