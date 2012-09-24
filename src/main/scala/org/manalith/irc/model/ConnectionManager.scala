a7b59df5dfdb6e3a4c198b2fb8d5782a8d66a5fa
mmx900 <mmx900@gmail.com>
2012-09-24 12:11:39 +0900

    코드 클린업

--- src/main/scala/org/manalith/irc/model/ConnectionManager.scala
+++ src/main/scala/org/manalith/irc/model/ConnectionManager.scala
@@ -1,33 +1,29 @@
 package org.manalith.irc.model
 
-import java.util.ArrayList
-
-import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.mutable.HashSet
 
 import org.manalith.irc.helper.LogHelper
 import org.pircbotx.MultiBotManager
-import org.pircbotx.PircBotX
 
 object ConnectionManager extends LogHelper {
 	lazy val manager = new MultiBotManager("ManalithIRC");
-	lazy val connections = new ArrayList[Connection]();
+	lazy val connections = new HashSet[Connection]();
 
 	def getConnection(server: Server): Connection = {
-		for (connection <- connections) {
-			if (connection.server == server)
+		connections.find(c => c.server == server) match {
+			case Some(c) => return c;
+			case None => {
+				val connection = new Connection(server, manager.createBot(server.hostname, server.port));
+				connections.add(connection);
+
 				return connection;
+			}
 		}
-
-		val connection = new Connection(server, manager.createBot(server.hostname, server.port));
-		connections.add(connection);
-
-		return connection;
 	}
 
 	def disconnectAllConnection(message: String) = {
-		for (conn <- connections) {
-			conn.quitServer(message);
-			conn.disconnect;
-		}
+		connections.foreach(c => {
+			c.quitServer(message);
+		});
 	}
 }
